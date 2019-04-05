28b85b6a558bbaed1ff4340c4e33c67f14ad39fc
zudi-lin <linzudi@g.harvard.edu>
2019-04-05 00:52:54 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,20 @@ from setuptools import find_packages
 import numpy as np
 
 def setup_package():
+
+    __version__ = '0.1'
+    url = 'https://github.com/zudi-lin/pytorch_connectomics'
+
     setup(name='vcg_connectomics',
-       version='0.1.0',
-       include_dirs=[np.get_include(), get_python_inc()], 
-       packages=find_packages()
+        version=__version__,
+        author='Zudi Lin',
+        author_email='linzudi@g.harvard.edu',
+        url=url,
+        license='MIT',
+        include_dirs=[np.get_include(), get_python_inc()], 
+        packages=find_packages()
     )
 
 if __name__=='__main__':
     # pip install --editable .
-	setup_package()
+    setup_package()
