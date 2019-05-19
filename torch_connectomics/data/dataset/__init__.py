ea09ccfc78bc4b804142d2c4f30d203ae4c77132
zudi-lin <linzudi@g.harvard.edu>
2019-05-19 16:48:06 -0400

    update docs

--- torch_connectomics/data/dataset/__init__.py
+++ torch_connectomics/data/dataset/__init__.py
@@ -1,8 +1,9 @@
 from .dataset_affinity import AffinityDataset
 from .dataset_synapse import SynapseDataset, SynapsePolarityDataset
-from .dataset_mito import MitoDataset
+from .dataset_mito import MitoDataset, MitoSkeletonDataset
 
 __all__ = ['AffinityDataset',
            'SynapseDataset',
            'SynapsePolarityDataset',
-           'MitoDataset']
+           'MitoDataset',
+           'MitoSkeletonDataset']
