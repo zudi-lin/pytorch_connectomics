da1e948714c4ef62bbf1f0c8e6c19d00008001da
donglaiw <donglai@holylogin02.rc.fas.harvard.edu>
2019-06-13 21:02:35 -0400

    remove build folder

--- build/lib.linux-x86_64-3.6/torch_connectomics/data/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .functional_collate import collate_fn, collate_fn_test, collate_fn_skel
--- build/lib.linux-x86_64-3.6/torch_connectomics/utils/seg/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import io_util, aff_util, seg_util, seg_eval, seg_malis, seg_core, \
-    seg_dist
