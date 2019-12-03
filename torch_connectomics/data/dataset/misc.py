148bb0c04b214f38020ca48f308dd033cb374efd
zudi-lin <linzudi@g.harvard.edu>
2019-12-03 00:27:18 -0500

    update dataload utils

--- torch_connectomics/data/dataset/misc.py
+++ torch_connectomics/data/dataset/misc.py
@@ -11,7 +11,7 @@ def count_volume(data_sz, vol_sz, stride):
     return 1 + np.ceil((data_sz - vol_sz) / stride.astype(float)).astype(int)
 
 def crop_volume(data, sz, st=(0, 0, 0)):  # C*D*W*H, C=1
-    st = np.array(st).astype(int)
+    st = np.array(st).astype(np.int32)
     return data[st[0]:st[0]+sz[0], st[1]:st[1]+sz[1], st[2]:st[2]+sz[2]]
 
 def crop_volume_mul(data, sz, st=(0, 0, 0)):  # C*D*W*H, for multi-channel input
