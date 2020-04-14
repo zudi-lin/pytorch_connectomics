f7958aa7b58f62d0581935f11fc69dd12a4b549c
Mourad Belhamissi <mouradbelo1@gmail.com>
2020-04-14 18:12:25 -0400

    update bash script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,6 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 && python train.py -t /n/pfister_lab2/Lab/zudilin/data/SNEMI3D/ -dn train_image.h5 -ln train_label.h5 -o outputs/unetv3 -lr 1e-03 --iteration-total 100000 --iteration-save 1000 -mi 18,160,160  -g 4 -c 8 -b 8 -ac  unetv3
+CUDA_VISIBLE_DEVICES=0, python train.py -i /n/home11/mbelhamissi/data/SNEMI3D/super_resolution/\
+    -o outputs/unetv3 -din train_downsampled.h5 -dln label_downsampled.h5 \
+    -lr 1e-03 --iteration-total 100000 --iteration-save 1000 -dp 0,0,0\
+    -mi 18,160,160 -ma unet_residual_3d -moc 3 \
+    -to 2 -lo 1 -wo 1 -g 1 -c 4 -b 4
+
