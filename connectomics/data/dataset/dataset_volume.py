45c1d3adb673706f4ef4d10eea74565c6baa4ca3
zudi-lin <linzudi@g.harvard.edu>
2020-04-18 22:28:31 -0400

    reject sampling in dataset

--- connectomics/data/dataset/dataset_volume.py
+++ connectomics/data/dataset/dataset_volume.py
@@ -41,11 +41,6 @@ class VolumeDataset(torch.utils.data.Dataset):
         self.sample_volume_size = np.array(sample_volume_size).astype(int)  # model input size
         if self.label is not None: 
             self.sample_label_size = np.array(sample_label_size).astype(int)  # model label size
-        #self.sample_label_size = np.array(sample_label_size).astype(int)  # model label size
-            # shrink input_size for valid label sample
-            for vid in range(len(self.input)):
-                self.input_size[vid] = np.minimum(self.input_size[vid],(np.array(self.label[vid].shape)/self.sample_label_size*self.sample_volume_size).astype(int))
-                
 
         # compute number of samples for each dataset (multi-volume input)
         self.sample_stride = np.array(sample_stride, dtype=int)
@@ -72,8 +67,7 @@ class VolumeDataset(torch.utils.data.Dataset):
 
         self.sample_num_a = np.sum(self.sample_num)
         self.sample_num_c = np.cumsum([0] + list(self.sample_num))
-        if self.label is not None:
-            self.label_vol_ratio = self.sample_label_size/self.sample_volume_size
+        self.label_vol_ratio = self.sample_label_size/self.sample_volume_size
 
         if mode=='test': # for test
             self.sample_size_vol = [np.array([np.prod(x[1:3]), x[2]]) for x in self.sample_size]
@@ -81,6 +75,41 @@ class VolumeDataset(torch.utils.data.Dataset):
 
     def __len__(self):  # number of possible position
         return self.sample_num_a
+
+    def __getitem__(self, index):
+        # orig input: keep uint format to save cpu memory
+        # sample: need float32
+
+        vol_size = self.sample_volume_size
+        if self.mode in ['train','valid']:
+            # train mode
+            # if elastic deformation: need different receptive field
+            # position in the volume
+            pos, out_input, out_label = self._rejection_sampling(vol_size, size_thres=-1, background=0, p=0.9):
+
+            # augmentation
+            if self.augmentor is not None:  # augmentation
+                # for warping: cv2.remap require input to be float32
+                # make labels index smaller. o/w uint32 and float32 are not the same for some values
+                data = {'image':out_input, 'label':out_label}
+                augmented = self.augmentor(data)
+                out_input, out_label = augmented['image'], augmented['label']
+            out_input = np.expand_dims(out_input, 0)
+            # output list
+            out_target = seg_to_targets(out_label, self.target_opt)
+            out_weight = seg_to_weights(out_target, self.weight_opt)
+
+            return pos, out_input, out_target, out_weight
+
+        elif self.mode == 'test':
+            # test mode
+            pos = self._get_pos_test(index)
+            out_input = (crop_volume(self.input[pos[0]], vol_size, pos[1:])/255.0).astype(np.float32)
+            return pos, np.expand_dims(out_input,0)
+
+    #######################################################
+    # Position Calculator
+    #######################################################
 
     def _index_to_dataset(self, index):
         return np.argmax(index < self.sample_num_c) - 1  # which dataset
@@ -130,45 +159,38 @@ class VolumeDataset(torch.utils.data.Dataset):
                 num_trial += 1
         pos[1:] = tmp_pos 
         return pos
-    
-    def __getitem__(self, index):
-        # orig input: keep uint format to save cpu memory
-        # sample: need float32
 
-        vol_size = self.sample_volume_size
-        if self.mode in ['train','valid']:
-            # train mode
-            # if elastic deformation: need different receptive field
-            # position in the volume
-            pos = self._get_pos_train(vol_size)
-            out_input = (crop_volume(self.input[pos[0]], vol_size, pos[1:])/255.0).astype(np.float32)
-            # position in the label 
-            pos_l = np.round(pos[1:]*self.label_vol_ratio)
-            out_label = crop_volume(self.label[pos[0]], self.sample_label_size, pos_l)
+    #######################################################
+    # Volume Sampler
+    #######################################################
 
-            if self.sample_invalid_thres[0]>0:
-                seg_bad = np.array([-1]).astype(out_label.dtype)[0]
-                out_mask = out_label!=seg_bad
-            else:
-                out_mask = torch.ones((1),dtype=torch.uint8)
-            
-            #out_label = relabel(out_label).astype(np.float32)
-            # augmentation
-            if self.augmentor is not None:  # augmentation
-                # for warping: cv2.remap require input to be float32
-                # make labels index smaller. o/w uint32 and float32 are not the same for some values
-                data = {'image':out_input, 'label':out_label}
-                augmented = self.augmentor(data)
-                out_input, out_label = augmented['image'], augmented['label']
-            out_input = np.expand_dims(out_input, 0)
-            # output list
-            out_target = seg_to_targets(out_label, self.target_opt)
-            out_weight = seg_to_weights(out_target, self.weight_opt)
+    def _rejection_sampling(self, vol_size, size_thres=-1, background=0, p=0.9):
+        while True:
+            pos, out_input, out_label = self._random_sampling(vol_size)
+            if size_thres > 0:
+                temp = (out_label!=background).astype(int).sum()
+                if temp > size_thres:
+                    break
+                elif random.random() > p:
+                    break
+            else: # no rejection sampling for the foreground mask
+                break
 
-            return pos, out_input, out_target, out_weight
+        return pos, out_input, out_label
 
-        elif self.mode == 'test':
-            # test mode
-            pos = self._get_pos_test(index)
-            out_input = (crop_volume(self.input[pos[0]], vol_size, pos[1:])/255.0).astype(np.float32)
-            return pos, np.expand_dims(out_input,0)
+    def _random_sampling(self, vol_size):
+        pos = self._get_pos_train(vol_size)
+        out_input = (crop_volume(self.input[pos[0]], vol_size, pos[1:])/255.0).astype(np.float32)
+        # position in the label 
+        pos_l = np.round(pos[1:]*self.label_vol_ratio)
+        out_label = crop_volume(self.label[pos[0]], self.sample_label_size, pos_l)
+
+        # The option of generating valid masks has been removed.
+        # if self.sample_invalid_thres[0]>0:
+        #     seg_bad = np.array([-1]).astype(out_label.dtype)[0]
+        #     out_mask = out_label!=seg_bad
+        # else:
+        #     out_mask = torch.ones((1),dtype=torch.uint8)
+
+        out_label = relabel(out_label).astype(np.float32)
+        return pos, out_input, out_label
