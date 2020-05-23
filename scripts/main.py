4cf8ce38b71671ced24402e9d31713c29c247528
Zijie Zhao <zijzhao@holylogin01.rc.fas.harvard.edu>
2020-05-22 21:06:33 -0400

    wrap inference_update_config

--- scripts/main.py
+++ scripts/main.py
@@ -6,6 +6,8 @@ from connectomics.config import get_cfg_defaults, save_all_cfg
 from connectomics.engine import Trainer
 
 def get_args():
+    r"""Get args from command lines.
+    """
     parser = argparse.ArgumentParser(description="Model Training & Inference")
     parser.add_argument('--config-file', type=str, help='configuration file (yaml)')
     parser.add_argument('--inference', action='store_true', help='inference mode')
@@ -20,7 +22,19 @@ def get_args():
     args = parser.parse_args()
     return args
 
+def update_inference_conifg(cfg):
+    r"""Update configurations (cfg) when case is inference.
+    """
+    cfg.MODEL.INPUT_SIZE = cfg.INFERENCE.INPUT_SIZE
+    cfg.MODEL.OUTPUT_SIZE = cfg.INFERENCE.OUTPUT_SIZE
+    cfg.DATASET.IMAGE_NAME = cfg.INFERENCE.IMAGE_NAME
+    cfg.DATASET.OUTPUT_PATH = cfg.INFERENCE.OUTPUT_PATH
+    cfg.DATASET.PAD_SIZE = cfg.INFERENCE.PAD_SIZE
+    cfg.SOLVER.SAMPLES_PER_BATCH = cfg.INFERENCE.SAMPLES_PER_BATCH
+
 def main():
+    r"""Main function.
+    """
     # arguments
     args = get_args()
     print("Command line arguments:")
@@ -31,16 +45,8 @@ def main():
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
 
-    # update configurations when case is inference:
     if args.inference:
-        cfg.MODEL.INPUT_SIZE = cfg.INFERENCE.INPUT_SIZE
-        cfg.MODEL.OUTPUT_SIZE = cfg.INFERENCE.OUTPUT_SIZE
-        cfg.MODEL.IN_PLANES = cfg.INFERENCE.IN_PLANES
-        cfg.MODEL.OUT_PLANES = cfg.INFERENCE.OUT_PLANES
-        cfg.DATASET.IMAGE_NAME = cfg.INFERENCE.IMAGE_NAME
-        cfg.DATASET.OUTPUT_PATH = cfg.INFERENCE.OUTPUT_PATH
-        cfg.DATASET.PAD_SIZE = cfg.INFERENCE.PAD_SIZE
-        cfg.SOLVER.SAMPLES_PER_BATCH = cfg.INFERENCE.SAMPLES_PER_BATCH
+        update_inference_conifg(cfg)
 
     cfg.freeze()
     print("Configuration details:")
