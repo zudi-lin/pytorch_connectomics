f463e45fc09cde3c6a27a4d589977a94bf4aff5c
zudi-lin <linzudi@g.harvard.edu>
2020-08-05 15:39:29 -0400

    debug augmentor for 2d

--- connectomics/data/augmentation/__init__.py
+++ connectomics/data/augmentation/__init__.py
@@ -41,32 +41,40 @@ def build_train_augmentor(cfg, keep_uncropped=False, keep_non_smoothed=False):
     #1. rotate
     if cfg.AUGMENTOR.ROTATE.ENABLED:
         aug_list.append(Rotate(p=cfg.AUGMENTOR.ROTATE.P))
+
     #2. rescale
     if cfg.AUGMENTOR.RESCALE.ENABLED:
         aug_list.append(Rescale(p=cfg.AUGMENTOR.RESCALE.P))
+
     #3. flip
     if cfg.AUGMENTOR.FLIP.ENABLED:
         aug_list.append(Flip(p=cfg.AUGMENTOR.FLIP.P, 
                              do_ztrans=cfg.AUGMENTOR.FLIP.DO_ZTRANS))
+
     #4. elastic
     if cfg.AUGMENTOR.ELASTIC.ENABLED:
         aug_list.append(Elastic(alpha=cfg.AUGMENTOR.ELASTIC.ALPHA, 
                                 sigma = cfg.AUGMENTOR.ELASTIC.SIGMA, 
                                 p=cfg.AUGMENTOR.ELASTIC.P))
+
     #5. grayscale
     if cfg.AUGMENTOR.GRAYSCALE.ENABLED:
         aug_list.append(Grayscale(p=cfg.AUGMENTOR.GRAYSCALE.P))
+
     #6. missingparts
     if cfg.AUGMENTOR.MISSINGPARTS.ENABLED:
         aug_list.append(MissingParts(p=cfg.AUGMENTOR.MISSINGPARTS.P))
+
     #7. missingsection
     if cfg.AUGMENTOR.MISSINGSECTION.ENABLED and not cfg.DATASET.DO_2D:
-            aug_list.append(MissingSection(p=cfg.AUGMENTOR.MISSINGSECTION.P, num_section=cfg.AUGMENTOR.MISSINGSECTION.NUM_SECTION))
+            aug_list.append(MissingSection(p=cfg.AUGMENTOR.MISSINGSECTION.P, 
+                                           num_sections=cfg.AUGMENTOR.MISSINGSECTION.NUM_SECTION))
+
     #8. misalignment
     if cfg.AUGMENTOR.MISALIGNMENT.ENABLED and not cfg.DATASET.DO_2D:
             aug_list.append(MisAlignment(p=cfg.AUGMENTOR.MISALIGNMENT.P, 
-                                     displacement=cfg.AUGMENTOR.MISALIGNMENT.DISPLACEMENT,
-                                     rotate_ratio=cfg.AUGMENTOR.MISALIGNMENT.ROTATE_RATIO))
+                                         displacement=cfg.AUGMENTOR.MISALIGNMENT.DISPLACEMENT,
+                                         rotate_ratio=cfg.AUGMENTOR.MISALIGNMENT.ROTATE_RATIO))
     #9. motion-blur
     if cfg.AUGMENTOR.MOTIONBLUR.ENABLED:
         aug_list.append(MotionBlur(p=cfg.AUGMENTOR.MOTIONBLUR.P, 
