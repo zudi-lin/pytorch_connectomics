98e8f7b4189d8566eb8407b501fb896a84f80452
zudi-lin <linzudi@g.harvard.edu>
2021-04-28 00:57:58 -0400

    fix update_inference_cfg

--- connectomics/config/utils.py
+++ connectomics/config/utils.py
@@ -59,13 +59,16 @@ def update_inference_cfg(cfg: CfgNode):
         cfg.MODEL.OUTPUT_SIZE = cfg.INFERENCE.OUTPUT_SIZE
 
     # output file name(s)
-    if cfg.DATASET.DO_CHUNK_TITLE or cfg.DATASET.INFERENCE.DO_SINGLY:
-        out_name = cfg.INFERENCE.OUTPUT_NAME
-        name_lst = out_name.split(".")
+    out_name = cfg.INFERENCE.OUTPUT_NAME
+    name_lst = out_name.split(".")
+    if cfg.DATASET.DO_CHUNK_TITLE or cfg.INFERENCE.DO_SINGLY:
         assert len(name_lst) <= 2, \
             "Invalid output file name is given."
         if len(name_lst) == 2:
             cfg.INFERENCE.OUTPUT_NAME = name_lst[0]
+    else:
+        if len(name_lst) == 1:
+            cfg.INFERENCE.OUTPUT_NAME = name_lst[0] + '.h5'
 
     for topt in cfg.MODEL.TARGET_OPT:
         # For multi-class semantic segmentation and quantized distance
@@ -126,7 +129,8 @@ def overwrite_cfg(cfg: CfgNode, args: argparse.Namespace):
 
 def validate_cfg(cfg: CfgNode):
     num_target = len(cfg.MODEL.TARGET_OPT)
-    assert len(cfg.INFERENCE.OUTPUT_ACT) == num_target
+    assert len(cfg.INFERENCE.OUTPUT_ACT) == num_target, \
+        "Activations need to be specified for each learning target."
 
 
 def convert_cfg_markdown(cfg):
