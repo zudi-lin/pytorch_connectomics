e71eb5a6615597798b497a15acebba09b6b54f64
Jason Ken Adhinarta <35003073+jasonkena@users.noreply.github.com>
2024-07-09 16:35:29 -0400

    bump Cython version to 0.29.22 to avoid _PyGen_Send problems in Python 3.10

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
     'scikit-image>=0.17.2',
     'opencv-python>=4.3.0',
     'matplotlib>=3.3.0',
-    'Cython==0.29.21',
+    'Cython>=0.29.22',
     'yacs>=0.1.8',
     'h5py>=2.10.0',
     'gputil>=1.4.0',
